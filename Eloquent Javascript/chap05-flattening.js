5a7cdec7c247d9c755e608f4d12242570f32f955
Henry Rylko <henryrylko@gmail.com>
2017-08-26 19:10:51 -0500

    First exercise of Chapter 5

---
+++ Eloquent Javascript/chap05-flattening.js
@@ -0,0 +1,19 @@
+/**
+  *  Use the reduce method in combination with the concat method to “flatten” an array of arrays
+  *  into a single array that has all the elements of the input arrays.
+  *
+  *  var arrays = [[1, 2, 3], [4, 5], [6]];
+  *  // Your code here.
+  *  // → [1, 2, 3, 4, 5, 6]
+  *
+  *
+  *  This exercise is from Chapter 5 of Eloquent JavaScript
+  *
+  */
+
+var arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce(function(accum, val) {
+    return accum.concat(val);
+}, []));
+
